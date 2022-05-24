4274e3dac78d5272d81e8597ac6e1bb9c22210af
Alina Tsabai <angelyna.ts@gmail.com>
2022-05-24 16:40:56 +0300

    Updates

--- src/redux/reducer/reducer.tsx
+++
@@ -1,8 +0,0 @@
-import {locationReducer} from "../slices/location";
-import {locationsReducer} from "../slices/locations";
-
-const reducer = {
-    locationsReducer,
-    locationReducer
-};
-export default reducer;
